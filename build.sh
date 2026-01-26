602633293170148948359d9e5668a98797dadcde
kylieeXD <khayloaftck@gmail.com>
2026-01-26 21:35:20 +0700

    rosemary: final destination

--- build.sh
+++ build.sh
@@ -1,80 +1,70 @@
-#!/bin/env bash
+#!/bin/bash
+SECONDS=0
+set -e
 
-# Define compile function
-function compile() {
-  # Load environment variables
-  source ~/.bashrc
-  source ~/.profile
+# Set kernel path
+KERNEL_PATH="out/arch/arm64/boot"
 
-  # Set environment variables
-  export LC_ALL=C
-  export USE_CCACHE=1
+# Set kernel file
+OBJ="${KERNEL_PATH}/Image"
+GZIP="${KERNEL_PATH}/Image.gz"
+CAT="${KERNEL_PATH}/Image.gz-dtb"
 
-  TANGGAL=$(date +"%Y%m%d-%H")
-  export ARCH=arm64
-  export KBUILD_BUILD_HOST=Rosemary
-  export KBUILD_BUILD_USER="MIUI56"
+# Set kernel name
+DATE="$(TZ=Asia/Jakarta date +%Y%m%d%H%M)"
+KERNEL_NAME="rethinking4.19-${DATE}.zip"
 
-  # Allocate 100GB of memory to ccache
-  ccache -M 100G
+# Create anykernel
+rm -rf anykernel
+git clone https://github.com/kylieeXD/AK3-Rosemary.git anykernel
 
-  # Install Kernel Dependencies
-  sudo apt update
-  sudo apt install -y libelf-dev libarchive-tools zstd flex bc ccache
+function KERNEL_COMPILE() {
+	# Set environment variables
+	export USE_CCACHE=1
+	export KBUILD_BUILD_HOST=builder
+	export KBUILD_BUILD_USER=khayloaf
 
-  # Download clang if not present
-  if [[ ! -d "clang" ]]; then mkdir clang && cd clang
-  bash <(curl -s https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman) -S
-  bash <(curl -s https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman) --patch=glibc
-  ls
-  cd ..
-  fi
+	# Create output directory and do a clean build
+	rm -rf out && mkdir -p out
 
-  # create output directory and do a clean or dirty build
-  read -p "Wanna do dirty build? (Y/N): " build_type
-  if [[ $build_type == "N" || $build_type == "n" ]]; then
-  echo Deleting out directory and doing clean Build
-  rm -rf out && mkdir -p out
-  fi
-  if [[ $build_type == "Y" || $build_type == "y" ]]; then
-  echo Warning :- Doing dirty build
-  fi
-  if ! [[ $build_type == "Y" || $build_type == "y" ]]; then
-  if ! [[ $build_type == "N" || $build_type == "n" ]]; then
-  echo Invalid Input , Read carefully before typing
-  echo Trying to restart script
-  . build.sh && exit
-  fi
-  fi
+	# Download clang if not present
+	if [[ ! -d "clang" ]]; then mkdir -p clang
+		wget https://github.com/Impqxr/aosp_clang_ci/releases/download/13289611/clang-13289611-linux-x86.tar.xz -O clang.tar.gz
+		tar -xf clang.tar.gz -C clang && if [ -d clang/clang-* ]; then mv clang/clang-*/* clang; fi && rm -rf clang.tar.gz
+	fi
 
-  # Build the kernel
-  make -j$(nproc --all) O=out ARCH=arm64 rosemary_defconfig
+	# Add clang bin directory to PATH
+	export PATH="${PWD}/clang/bin:$PATH"
 
-  # Add clang bin directory to PATH
-  PATH="${PWD}/clang/bin:${PATH}"
+	# Make the config
+	make O=out ARCH=arm64 rosemary_defconfig
 
-  # Build the kernel with clang and log output
-  make -j$(nproc --all) O=out CC="clang" LLVM=1 CONFIG_NO_ERROR_ON_MISMATCH=y 2>&1 | tee build.log
+	# Build the kernel with clang and log output
+	make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_COMPAT=arm-linux-gnueabi- LLVM=1 LLVM_IAS=1 2>&1 | tee -a out/compile.log
 }
 
-function zupload()
-{
-zimage=out/arch/arm64/boot/Image.gz-dtb
-if ! [ -a $zimage ];
-then
-echo  " Failed to compile zImage, fix the errors first "
-else
-echo -e " Build succesful, generating flashable zip now "
-rm -rf AnyKernel
-git clone --depth=1 https://github.com/MIUI56/AnyKernel3_rosemary AnyKernel
-cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
-cd AnyKernel
-zip -r9 Rosemary-${TANGGAL}.zip *
-curl -L bashupload.com -T Rosemary-${TANGGAL}.zip
-cd ../
-fi
+function KERNEL_RESULT() {
+	# Check if build is successful
+	if [ ! -f "$OBJ" ] || [ ! -f "$GZIP" ] || [ ! -f "$CAT" ]; then
+		exit 1
+	fi
+
+	# Copying image
+	cp "$CAT" "$1/kernels/"
+
+	# Created zip kernel
+	cd "$1" && zip -r9 "$2" *
+
+	# Upload kernel
+	curl -T "$2" -u :dc4f2d6d-ef86-4241-af44-44f311a0ecb9 https://pixeldrain.com/api/file/
+
+	# Back to kernel root
+	cd -
 }
 
-# Run functions
-compile
-zupload
+# Run functions for R variant
+KERNEL_COMPILE "$1"
+KERNEL_RESULT anykernel "$KERNEL_NAME"
+
+# Done bang
+echo -e "Completed in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !\n"
